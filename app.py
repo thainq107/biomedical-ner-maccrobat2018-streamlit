8592f364717e0f7f4744e439f73b5cf99a75e779
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2025-02-01 10:39:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,17 @@ classifier = pipeline(
 )
 
 def main():
-  st.title('Biomedical NER')
-  st.title('Model: DistilBERT. Dataset: MACCROBAT2018')
-  sentence = """A 48 year - old female presented with vaginal bleeding and abnormal Pap smears .
-  Upon diagnosis of invasive non - keratinizing SCC of the cervix ,
-  she underwent a radical hysterectomy with salpingo - oophorectomy
-  which demonstrated positive spread to the pelvic lymph nodes and the parametrium .
-  Pathological examination revealed that the tumour also extensively involved the lower uterine segment .
-  """
-  text_input = st.text_input("Sentence: ", sentence)
-  results = classifier(text_input)
-  for result in results:
-      entity = result["entity_group"]
-      word = result["word"]
-      st.success(f'Word: {word} - Entity {entity}') 
+    st.title('Biomedical NER')
+    st.title('Model: DistilBERT. Dataset: MACCROBAT2018')
+    sentence = """A 48 year - old female presented with vaginal bleeding and abnormal Pap smears .
+      Upon diagnosis of invasive non - keratinizing SCC of the cervix ,
+      she underwent a radical hysterectomy with salpingo - oophorectomy
+      which demonstrated positive spread to the pelvic lymph nodes and the parametrium .
+      Pathological examination revealed that the tumour also extensively involved the lower uterine segment .
+      """
+    text_input = st.text_input("Sentence: ", sentence)
+    results = classifier(text_input)
+    st.success(results) 
 
 if __name__ == '__main__':
      main() 
